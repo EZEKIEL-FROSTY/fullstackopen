7803438bda63be252d56f48fb88182b2616c978b
Ezekiel Lim <limezekiel9@gmail.com>
2025-08-17 17:03:58 +0800

    2.11 Phonebook step 6 setting up db.json and useEffect

--- week2/phonebook/src/App.jsx
+++ week2/phonebook/src/App.jsx
@@ -1,4 +1,5 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 
 const Person = ( {person} ) => {
   return(
@@ -48,15 +49,18 @@ const Filter = ( {value, onChange} ) => {
 }
 
 const App = () => {
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ])
+  const [persons, setPersons] = useState([])
   const [newName, setNewName] = useState('')
   const [newNumber, setNewNumber] = useState('')
   const [newFilter, setNewFilter] = useState('')
+
+  useEffect(() => {
+    const eventHandler = response => {
+      setPersons(response.data)
+    }
+    const promise = axios.get('http://localhost:3001/persons')
+    promise.then(eventHandler)
+  }, [])
   
   const handleAddButton = (event) => {
     event.preventDefault()
@@ -79,7 +83,6 @@ const App = () => {
   return (
     <div>
       <h2>Phonebook</h2>
-
       <Filter value={newFilter} onChange={(event) => setNewFilter(event.target.value)}/>
       
       <h3> add a new </h3>
