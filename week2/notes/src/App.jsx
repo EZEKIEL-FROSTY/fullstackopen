8f9f8e6cb57e3760f722a483f6ede17e8e085a49
Ezekiel Lim <limezekiel9@gmail.com>
2025-08-29 16:29:21 +0800

    Clean up

--- week2/notes/src/App.jsx
+++ week2/notes/src/App.jsx
@@ -1,12 +1,14 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import Notification from './components/Notification'
 import noteService from './services/notes'
+import Footer from './components/Footer'
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('some error happened...')
 
   useEffect(() => {
     noteService
@@ -26,9 +28,12 @@ const App = () => {
         setNotes(notes.map(note => note.id === id ? returnedNote : note))
       })
       .catch(error => {
-        alert(
-          `the note '${note.content}' was already deleted from server`
+        setErrorMessage(
+          `Note '${note.content}' was already deleted from server`
         )
+        setTimeout(() => {
+          setErrorMessage(null)
+        }, 5000)
         setNotes(notes.filter(n => n.id !== id))
       })
   }
@@ -55,12 +60,14 @@ const App = () => {
     // and event.target.value refers to the input value of the element
   }
 
+  // notesToShow gets the value of notes
   const notesToShow = showAll ? notes :
   notes.filter(note => note.important)
 
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message = {errorMessage}/>
       <div>
         <button onClick={() => setShowAll(!showAll)}>
           show {showAll ? 'important' : 'all'}
@@ -81,6 +88,7 @@ const App = () => {
         />
         <button type='submit'>save</button>
       </form>
+      <Footer />
     </div>
   )
 }
--- week3/index.js
+++ week3/index.js
@@ -3,9 +3,10 @@
 //const http = require('http') //--> imports Node's built in web server module
 
 const express = require('express')
+const cors = require('cors')
 const app = express()
 
-app.use(express.json())
+app.use(cors())
 
 let notes = [
     {
@@ -24,6 +25,17 @@ let notes = [
         important : true
     }
 ]
+
+const requestLogger = (request, response, next) => {
+    console.log('Method:', request.method)
+    console.log('Path:  ', request.path)
+    console.log('Body:  ', request.body)
+    console.log('---')
+    next()
+}
+
+app.use(express.json())
+app.use(requestLogger)
 
 app.get('/', (request, response) => {
     response.send('<h1>Hello World!</h1?')
@@ -81,6 +93,12 @@ app.post('/api/notes', (request, response) => {
     response.json(note)
 })
 
+const unknownEndpoint = (request, response) => {
+    response.status(404).send({ error: 'unknown endpoint' })
+}
+app.use(unknownEndpoint)
+
 const PORT = 3001
-app.listen(PORT)
-console.log(`Server running on port ${PORT}`)
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
